b829a7d383b6a49eb43951f64df6975d095c9fe3
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-09-30 19:15:26 +0200

    update

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 const LEVELS = [
   `
-  ..............||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||..........
-  ..............|||||||||||||||||||||||!!!!|||||||||||||||||||||||||||||||||||||||||||||||||||..........
-  ..............|||||||||||||||||||||||||||||||||||.......||||||..................||||||||||||..........
-  ..................|||||||||..........||||||.......||..|||||||||..|||||||....||||||||...|||||..........
-  @.................|||||||.....||||........|....|..|....|||||||...|||||||||......||||............+.....
-  ..................|...............||...||......|........||||||..||||||||||......||||....||||..........
-  ..............|............|........|....|.....||..|.................|||||.........|..||||||..........
-  ..............|........||||||||||||||....|.....|..||||||||||||.......|||||......|.....||||||..........
-  ..............||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||..........`,
+..............||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||..........
+..............|||||||||||||||||||||||!!!!|||||||||||||||||||||||||||||||||||||||||||||||||||..........
+..............|||||||||||||||||||||||||||||||||||.......||||||..................||||||||||||..........
+..................|||||||||..........||||||.......||..|||||||||..|||||||....||||||||...|||||..........
+.@................|||||||.....||||........|....|..|....|||||||...|||||||||......||||............+.....
+..................|...............||...||......|........||||||..||||||||||......||||....||||..........
+..............|............|........|....|.....||..|.................|||||.........|..||||||..........
+..............|........||||||||||||||....|.....|..||||||||||||.......|||||......|.....||||||..........
+..............||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||..........`,
 ];
 
-let scale = 35;
+let scale = 40;
 
 let Level = class Level {
   constructor(plan) {
@@ -68,6 +68,43 @@ Player.prototype.size = new Vector(0.8, 0.5);
 
 var playerSpeed = 7;
 
+Player.prototype.update = function (time, keys) {
+  let pos = this.pos;
+  let xSpeed = 0;
+  if (keys.ArrowLeft) xSpeed -= time * playerSpeed;
+  if (keys.ArrowRight) xSpeed += time * playerSpeed;
+  let movedX = pos.plus(new Vector(xSpeed, 0));
+  pos = movedX;
+
+  let ySpeed = 0;
+  if (keys.ArrowUp) ySpeed -= time * playerSpeed;
+  if (keys.ArrowDown) ySpeed += time * playerSpeed;
+  let movedY = pos.plus(new Vector(0, ySpeed));
+  pos = movedY;
+
+  this.pos = pos;
+};
+
+function trackKeys(keys) {
+  let down = Object.create(null);
+  function track(event) {
+    if (keys.includes(event.key)) {
+      down[event.key] = event.type == "keydown";
+      event.preventDefault();
+    }
+  }
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+  return down;
+}
+
+const arrowKeys = trackKeys([
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowUp",
+  "ArrowDown",
+]);
+
 const levelMap = {
   ".": "empty",
   "|": "rock",
@@ -112,6 +149,8 @@ CanvasDisplay.prototype.clearDisplay = function () {
 
 let level = new Level(LEVELS[0]);
 
+console.log(level.player);
+
 function animate(deltaTimeFunc) {
   let lastTime = 0;
   const frame = (timeStamp) => {
@@ -127,10 +166,11 @@ function animate(deltaTimeFunc) {
 
 let canv = new CanvasDisplay(document.body, level);
 
-//console.log(canv);
-
 animate((deltaTime) => {
   canv.clearDisplay();
   canv.drawPlayer(level.player);
-  //level.player = level.player.update(deltaTime, level, arrowKeys, canv.viewport);
+  level.player.update(
+    deltaTime,
+    arrowKeys
+  );
 });
